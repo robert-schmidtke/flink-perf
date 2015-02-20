0e69bf40da0d802529fcbd655a9dbba5912c9dba
Robert Schmidtke <ro.schmidtke@gmail.com>
2015-02-20 16:25:44 +0100

    time measurement and accumulators for spark grep job

--- spark-jobs/src/main/java/com/github/projectflink/spark/Grep.java
+++ spark-jobs/src/main/java/com/github/projectflink/spark/Grep.java
@@ -1,21 +1,23 @@
 package com.github.projectflink.spark;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.spark.Accumulator;
-import org.apache.spark.AccumulatorParam;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Grep {
 
 	public static void main(String[] args) {
+		long start = System.nanoTime();
+		
 		String master = args[0];
 		String inFile = args[1];
 		String outFile = args[2];
@@ -29,19 +31,24 @@ public class Grep {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
 		JavaRDD<String> file = sc.textFile(inFile);
+		final Map<String, Accumulator<Integer>> accums = new HashMap<String, Accumulator<Integer>>();
 		for(int p = 0; p < patterns.length; p++) {
 			final String pattern = patterns[p];
+			accums.put("filterMatchCount-" + pattern, sc.accumulator(0));
+			accums.put("filterRecordCount-" + pattern, sc.accumulator(0));
 			JavaRDD<String> res = file.filter(new Function<String, Boolean>() {
 				private static final long serialVersionUID = 1L;
 				Pattern p = Pattern.compile(pattern);
 
 				@Override
 				public Boolean call(String value) throws Exception {
+					accums.get("filterRecordCount-" + pattern).add(1);
 					if (value == null || value.length() == 0) {
 						return false;
 					}
 					final Matcher m = p.matcher(value);
 					if (m.find()) {
+						accums.get("filterMatchCount-" + pattern).add(1);
 						return true;
 					}
 					return false;
@@ -49,5 +56,12 @@ public class Grep {
 			});
 			res.saveAsTextFile(outFile+"_"+pattern);
 		}
+		
+		long duration = System.nanoTime() - start;
+		for(String pattern : patterns) {
+			System.err.println("filterMatchCount-" + pattern + ": " + accums.get("filterMatchCount-" + pattern).value());
+			System.err.println("filterRecordCount-" + pattern + ": " + accums.get("filterRecordCount-" + pattern).value());
+		}
+		System.err.println("Runtime: " + (duration / 1000000) + "ms");
 	}
 }
